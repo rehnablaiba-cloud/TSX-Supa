28c75d3c83c8eabb1de421c02aea905cb0eafdae
rehnablaiba-cloud <rehnab.laiba@gmail.com>
2026-03-31 17:58:43 +0000

    jjjj

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { useAuth } from "./context/AuthContext";
-import { useQuery } from "@apollo/client";
 import { ThemeProvider } from "./context/ThemeContext";
 import LoginPage from "./components/Auth/LoginPage";
 import Sidebar from "./components/Layout/Sidebar";
