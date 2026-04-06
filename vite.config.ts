83be4b27991f2aaa64747485e803cb37f67b4519
rehnablaiba-cloud <rehnab.laiba@gmail.com>
2026-04-06 10:29:49 +0000

    vv

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { VitePWA } from 'vite-plugin-pwa'
-
-export default defineConfig({
-  plugins: [react()],
-  resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
-});
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({ registerType: 'autoUpdate' })
-  ]
-})
+    VitePWA({ registerType: 'autoUpdate' }),
+  ],
+  resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
+});
